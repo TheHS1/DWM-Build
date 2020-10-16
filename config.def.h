d29d61fd4a54429bbf0f7c98fcdd29212655b1fa
harjot singh <harjot.singh0712@gmail.com>
2020-10-16 16:19:36 -0700

    switched volume and brightness signals to goblocks instead of dwmblocks

--- config.def.h
+++ config.def.h
@@ -111,11 +111,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ 0, XF86XK_AudioLowerVolume, spawn, SHCMD("amixer set Master 2%- && volnoti-show $(amixer get Master | grep -Po '[0-9]+(?=%)' | head -1) && amixer set Master unmute && pkill -RTMIN+2 dwmblocks" ) },
-        { 0, XF86XK_AudioMute, spawn, SHCMD("amixer set Master toggle && if amixer get Master | grep -Fq '[off]'; then volnoti-show -m; else volnoti-show $(amixer get Master | grep -Po '[0-9]+(?=%)' | head -1); fi && pkill -RTMIN+2 dwmblocks" ) },
-        { 0, XF86XK_AudioRaiseVolume, spawn, SHCMD("amixer set Master 2%+ && volnoti-show $(amixer get Master | grep -Po '[0-9]+(?=%)' | head -1) && amixer set Master unmute && pkill -RTMIN+2 dwmblocks") },
-        { 0, XF86XK_MonBrightnessUp, spawn, SHCMD("light -A 5 && pkill -RTMIN+1 dwmblocks") },
-        { 0, XF86XK_MonBrightnessDown, spawn, SHCMD("light -U 5 && pkill -RTMIN+1 dwmblocks") },
+	{ 0, XF86XK_AudioLowerVolume, spawn, SHCMD("amixer set Master 2%- && volnoti-show $(amixer get Master | grep -Po '[0-9]+(?=%)' | head -1) && amixer set Master unmute && pkill -RTMIN+2 goblocks" ) },
+        { 0, XF86XK_AudioMute, spawn, SHCMD("amixer set Master toggle && if amixer get Master | grep -Fq '[off]'; then volnoti-show -m; else volnoti-show $(amixer get Master | grep -Po '[0-9]+(?=%)' | head -1); fi && pkill -RTMIN+2 goblocks" ) },
+        { 0, XF86XK_AudioRaiseVolume, spawn, SHCMD("amixer set Master 2%+ && volnoti-show $(amixer get Master | grep -Po '[0-9]+(?=%)' | head -1) && amixer set Master unmute && pkill -RTMIN+2 goblocks") },
+        { 0, XF86XK_MonBrightnessUp, spawn, SHCMD("light -A 5 && pkill -RTMIN+1 goblocks") },
+        { 0, XF86XK_MonBrightnessDown, spawn, SHCMD("light -U 5 && pkill -RTMIN+1 goblocks") },
         { MODKEY|ShiftMask, XK_x, spawn, SHCMD("~/Scripts/lock") },  
         { MODKEY|ShiftMask, XK_s, spawn, SHCMD("spectacle -bcr") },
 };
