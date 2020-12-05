c6a265bd3ccb7ab1c78568236d3cd695e2e2eb9b
Harjot Singh <harjot.singh0712@gmail.com>
2020-12-04 18:06:17 -0800

    Changed dmenu colors

--- config.def.h
+++ config.def.h
@@ -74,7 +74,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", "#1d1f21", "-nf", "#d8dee9", "-sb", "#444", "-sf", "#268bd2", NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
 #include <X11/XF86keysym.h>
